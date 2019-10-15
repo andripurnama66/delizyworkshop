a2b6cfd3942b31154fae11a3721329dcb016b5f9
andripurnama66 <kharismandrifp@gmail.com>
2019-10-16 00:21:04 +0700

    carousel component

---
+++ components/Carousel.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="bg-overlay">
+            <div class="container">
+            <div class="row justify-content-center">
+
+                    
+                
+                <div class="col-md-6">
+                    <img src="/delizy.png" class="my-5 float-right" width="200px" height="200px" alt="Delizy Indonesia">
+                </div>
+                <div class="col-md-6">
+                    <img src="/Ubaya.jpg" class="my-5" width="200px" height="200px" alt="Delizy Indonesia">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center text-white">Presents :</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center text-white">How to make Water Kefir</p>
+                </div>
+            </div>
+            </div>
+        </div>
+    
+    
+    </div>
+</template>
+<style scoped>
+.full-width{
+    
+    width: 100%;
+    height: 100vh;
+}
+.bg-overlay{
+    background: linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url("https://images.unsplash.com/photo-1551040078-5a2f375f9549?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    color: #fff;
+    height: 100vh;
+    padding-top: 50px;
+}
+.overlay-div{
+   height:100vh;
+   width: 100%;
+   position:absolute;
+   background-color:#000;
+   opacity: 0.7;
+}
+h1 {
+    font-size: 3rem;
+}
+p {
+    font-size: 2rem;
+}
+</style>
+<script>
+export default {
+    
+}
+</script>

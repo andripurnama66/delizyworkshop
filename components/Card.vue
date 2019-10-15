ad11d9861f002906409455b895dfa22069e39d33
andripurnama66 <kharismandrifp@gmail.com>
2019-10-16 00:20:51 +0700

    card article concept

---
+++ components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+   <div>
+       <div class="card" v-for="card in cards" :key="'kartu.id'">
+            <img src="#" class="card-img-top" :alt="'card.title'">
+            <div class="card-body">
+                <p class="card-text">{{ card.textArticle }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    data() {
+        return {
+            cards : [
+                { id: 1, title : '', src: '', textArticle: 'Lorem ipsum dolor sit amet' },
+                { id: 2, title : '', src: '', textArticle: '' },
+                { id: 3, title : '', src: '', textArticle: '' },
+            ]
+        }
+    }
+}
+</script>

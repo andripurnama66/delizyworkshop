3b3f167cfb1c34d2075466d41ad370c20fa823c7
andripurnama66 <kharismandrifp@gmail.com>
2019-10-16 00:21:25 +0700

    navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
+  <div class="navbar-brand">
+  <nuxt-link to="/">
+  <img src="/logoicon.png" alt="Delizy Indonesia">
+  </nuxt-link>
+  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Venue</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Mentor</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">News</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Reservation</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    
+}
+</script>

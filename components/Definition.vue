115c9bce994c075a3b78b202629d2cd33d932d9d
andripurnama66 <kharismandrifp@gmail.com>
2019-10-16 00:21:13 +0700

    water kefir definition

---
+++ components/Definition.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="row h-100 no-gutters">
+        <div class="col-md-6 bg-success">
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <h2 class="text-center text-white">Apa itu Water Kefir?</h2>
+            <p class="text-center text-white mt-5">Water kefir merupakan minuman probiotik yang mengandung berbagai macam asam dan enzim yang berguna, memiliki berbagai manfaat baik untuk pencernaan maupun sebagai pengobatan.</p>
+            
+        </div>
+        <div class="col-md-6">
+            <img src="/kiwii.jpg" class="background-kefir" alt="kiwi kefir">
+        </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.background-kefir {
+    widows: 100%;
+    min-height: 100vh;
+}
+h2 {
+    font-size: 2rem;
+}
+p {
+    font-size: 1.5rem;
+}
+</style>
+<script>
+export default {
+    
+}
+</script>

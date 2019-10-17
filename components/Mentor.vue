ec8663e89fcf46754b22d5c88aed67d178ff72ef
andripurnama66 <kharismandrifp@gmail.com>
2019-10-18 00:24:44 +0700

    updated

---
+++ components/Mentor.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="mentor">
+        
+            <div class="row justify-content-center">
+                <div class="col-md-6 offset-md-3 mt-2">
+                    
+                    
+                    <img src="/mentor.jpg" class="rounded-circle text-center" width="200px" height="200px" alt="Delizy Water Kefir Mentor Ruth Chrisnasari">
+                    <h2 class="mt-3"><a href="https://id.linkedin.com/in/ruth-chrisnasari-07aa6743"> Chrisnasari, S.TP., M.P.</a> </h2>
+                    <p>Dosen Universitas Surabaya</p>
+                </div>
+                
+            </div>
+        
+        
+    </div>
+</template>
+<style scoped>
+p
+{
+    font-size: 1.5rem;
+}
+</style>
+<script>
+export default {
+    
+}
+</script>

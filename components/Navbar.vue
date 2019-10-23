061fa16ad33a98b17b0f36082a278c02fc4af288
andripurnama66 <kharismandrifp@gmail.com>
2019-10-23 22:44:09 +0700

    change for mobile responsive

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,33 +1,25 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
-  <div class="navbar-brand">
-  <nuxt-link to="/">
-  <img src="/logoicon.png" width="63px" height="63px" alt="Delizy Indonesia">
-  </nuxt-link>
-  </div>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#venue">Venue</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#mentor">Mentor</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#articles">Article</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#Registration">Reservation</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<div>
+  <b-navbar toggleable="lg" variant="light">
+    <b-navbar-brand href="#"><img src="/delizy.png" width="63px" height="63px" alt="Delizy Indonesia"></b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    
+ <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item v-scroll-to="'#venue'">Venue</b-nav-item>
+        <b-nav-item v-scroll-to="'#mentor'">Mentor</b-nav-item>
+        <b-nav-item v-scroll-to="'#articles'">Articles</b-nav-item>
+        <b-nav-item v-scroll-to="'#registration'">Register</b-nav-item>
+        
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
 </template>
 <style scoped>
 

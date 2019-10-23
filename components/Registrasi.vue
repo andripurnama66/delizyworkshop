d22f744dafd04fca85d0bd7b484fad2c36aa313b
andripurnama66 <kharismandrifp@gmail.com>
2019-10-23 23:28:32 +0700

    change layout registration

--- components/Registrasi.vue
+++ components/Registrasi.vue
@@ -3,17 +3,51 @@
         <section>
             
                 <div class="row justify-content-center">
-                    <div class="col-md-6 offset-md-2">
-                        <h4>Pendaftaran Masih dibuka. Slot Terbatas</h4>
-                        <a href="https://www.bukalapak.com/p/tiket-voucher/tiket-voucher-lainnya/2c8zk5o-jual-pelatihan-pembuatan-water-kefir-dan-gratis-starter-biakan-probiotic?from=product_owner&product_owner=normal_seller" target="_blank" class="btn btn-success btn-lg mt-5">Daftar</a>
+                    <div class="col-md-6">
+                        <div class="card border-success shadow">
+                            
+                            <div class="card-body">
+                                <h2 class="card-title text-center">Early Bird Package</h2>
+                                <h5 class="pl-2 pt-2">Apa yang akan anda dapatkan ?</h5>
+                                <ul class="list-group list-group-flush pt-3">
+                                    <li class="list-group-item">Stater Biakan Probiotic </li>
+                                    <li class="list-group-item">Sertifikat</li>
+                                    <li class="list-group-item">Water Kefir</li>
+                                </ul>
+                                <i class="pt-4">Persediaan terbatas. Sebelum 10 November 2019</i>
+                                <h4 class="text-center text-danger mt-3 strikethrough">Rp 300.000,-</h4>
+                                <h2 class="text-center mt-2">Rp 200.000,-</h2>
+                                <a href="https://www.bukalapak.com/p/tiket-voucher/tiket-voucher-lainnya/2c8zk5o-jual-pelatihan-pembuatan-water-kefir-dan-gratis-starter-biakan-probiotic?from=product_owner&product_owner=normal_seller" class="btn btn-lg btn-success container mt-4">Beli Sekarang !</a>
+                            </div>
+                        </div>
                     </div>
+                    <div class="col-md-6">
+                        <div class="card border-danger shadow">
+                            
+                            <div class="card-body">
+                                <h2 class="card-title text-center">Normal Package</h2>
+                                <h5 class="pl-2 pt-2">Apa yang akan anda dapatkan ?</h5>
+                                <ul class="list-group list-group-flush pt-3">
+                                    <li class="list-group-item">Stater Biakan Probiotic </li>
+                                    <li class="list-group-item">Sertifikat</li>
+                                    <li class="list-group-item">Water Kefir</li>
+                                </ul>
+                                
+                                <h2 class="text-center mt-4">Rp 300.000,-</h2>
+                                <a href="https://www.bukalapak.com/p/tiket-voucher/tiket-voucher-lainnya/2c8zk5o-jual-pelatihan-pembuatan-water-kefir-dan-gratis-starter-biakan-probiotic?from=product_owner&product_owner=normal_seller" class="btn disabled btn-secondary btn-lg container mt-4" aria-disabled="true">Beli Sekarang !</a>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             
         </section>
     </div>
 </template>
 <style scoped>
-
+.strikethrough {
+    text-decoration: line-through;
+}
 </style>
 <script>
 export default {
